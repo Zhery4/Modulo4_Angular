149d5e764bca2245155c4d846d481c6b22f03fd0
Zhery <alejandroramirezescalona34@gmail.com>
2024-11-10 22:30:32 +0100

    add new components

--- LabAngular/src/app/app.module.ts
+++ LabAngular/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { PublicMenuComponent } from './common/public-menu/public-menu.component';
+import { PrivateMenuComponent } from './common/private-menu/private-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    PublicMenuComponent,
+    PrivateMenuComponent
   ],
   imports: [
     BrowserModule,
